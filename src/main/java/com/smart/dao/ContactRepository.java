d8c577a17269f4dac07a8dd0c13106e002caf4c9
RishabhPatel3005 <rishabhpatel3005@gmail.com>
2024-07-10 17:52:56 +0530

    update with new changes

--- src/main/java/com/smart/dao/ContactRepository.java
+++ src/main/java/com/smart/dao/ContactRepository.java
@@ -9,9 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.Contact;
+import com.smart.entities.User;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer>{
 
 	@Query("from Contact as c where c.user.id =:userId")
 	public Page<Contact> findContactByUser(@Param("userId") int userId, Pageable pageable);
+	
+	public List<Contact> findByNameContainingAndUser(String name,User user);
 }
